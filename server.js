1b5791c6684290fc3b40bd2cc1c233c76fa2c696
abodh5921 <abodh5921@gmail.com>
2025-06-17 21:59:50 +0530

    initial setup

---
+++ server.js
@@ -0,0 +1,6 @@
+const app = require("./app");
+const http = require("http");
+const server = http.createServer(app);
+server.listen(process.env.PORT || 8000, () => {
+    console.log(`Server is running on port ${process.env.PORT || 8000}`);
+});
