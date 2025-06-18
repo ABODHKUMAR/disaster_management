e76c9692a79fd079d31d152de46310150a020706
abodh5921 <abodh5921@gmail.com>
2025-06-18 10:08:58 +0530

    add functionality to CRUD disaster

---
+++ backend/routes/disasterRoutes.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const router = express.Router();
+const {
+  createDisaster,
+  getDisasters,
+  updateDisaster,
+  deleteDisaster,
+} = require("../controllers/disasterController");
+
+router.post("/", createDisaster);
+
+router.get("/", getDisasters);
+
+router.put("/:id", updateDisaster);
+
+router.delete("/:id", deleteDisaster);
+
+module.exports = router;
